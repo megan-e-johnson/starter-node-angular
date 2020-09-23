b2abdf996e01f9edd068501cbbd27309ba0a5d58
Megan E. Johnson <megane.johnson@Megans-MBP.hsd1.ca.comcast.net>
2020-09-23 15:58:42 -0700

    get running

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.196.33.129',
+      host: '3.81.166.19',
       username: 'ubuntu',
       privateKey: 'fp-key.pem'
     })
